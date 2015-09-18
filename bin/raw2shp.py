2e835346fadffadfde726ba7bca5d61c1342a260
Yann Chemin <ychemin@gmail.com>
2015-09-18 22:40:52 +0200

    First version of the conversion module file, output in WKT and SHP

--- bin/raw2shp.py
+++ bin/raw2shp.py
@@ -18,7 +18,24 @@ b = np.genfromtxt(r'results.txt', delimiter=',', names=True, dtype=None)
 print( b[0][3].split('=')[-1], b[0][4].split('=')[-1])
 print( b.shape[0] )
 
-from osgeo import ogr
+#Final stage: Create a new shapefile directly
+import os, osgeo.ogr, osgeo.osr
+sr = osgeo.osr.SpatialReference()
+#Assumes Geo lat/long wgs84
+sr.ImportFromEPSG(3148)
+#Export to Shapefile called craters.shp
+driver = osgeo.ogr.GetDriverByName('ESRI Shapefile')
+if os.path.exists('craters.shp'):
+        driver.DeleteDataSource('craters.shp')
+shpData = driver.CreateDataSource('craters.shp')
+if shpData is None:
+        print ' Could not create file'
+        sys.exit(1)
+lyr = shpData.CreateLayer('layer1', sr, osgeo.ogr.wkbPolygon)
+lyrDef = lyr.GetLayerDefn()
+idField = osgeo.ogr.FieldDefn("ID_0", osgeo.ogr.OFTReal)
+lyr.CreateField(idField)
+fidx = 0
 
 i = 0
 while (i+1 < b.shape[0]):
@@ -28,13 +45,29 @@ while (i+1 < b.shape[0]):
 	x2 = float(b[i+1][3].split('=')[-1])
 	y2 = float(b[i+1][4].split('=')[-1])
 	wkt = "POINT (" + str(x1) +" " + str(y1) + ")"
-	pt = ogr.CreateGeometryFromWkt(wkt)
+	pt = osgeo.ogr.CreateGeometryFromWkt(wkt)
 	bufferDistance = np.sqrt(pow(x2-x1,2)+np.sqrt(pow(y2-y1,2)))
 	print( bufferDistance)
 	poly = pt.Buffer(bufferDistance)
 	print( "%s buffered by %d is %s" % (pt.ExportToWkt(), bufferDistance, poly.ExportToWkt()))
 	#Make sure we skip to the next tuple
 	i = i+2
+	##Now import it into a shapefile
+        ftr = osgeo.ogr.Feature(lyrDef)
+        ftr.SetGeometry(poly)
+        ftr.SetFID(fidx)
+        ftr.SetField(ftr.GetFieldIndex('ID_0'),i/2)
+        lyr.CreateFeature(ftr)
+        fidx += 1
 
-##Now import it into a shapefile
+shpData.Destroy()
+
+
+#create shapefile projection file
+sr.MorphToESRI()
+file = open('craters.prj', 'w')
+file.write(sr.ExportToWkt())
+file.close()
+
+
  
