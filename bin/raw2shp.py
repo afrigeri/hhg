b0bf1d55372cf15f0b18c725ad1cb651e90da372
Yann Chemin <ychemin@gmail.com>
2015-09-18 22:00:26 +0200

    Initialising the conversion module file

---
+++ bin/raw2shp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+#Purpose:
+#Convert raw results from the Holy Hand Grenade
+#to a shapefile of crater circles (polygons)
+#This is using the buffer option from OGR
+#The first line has the center point location
+#The second line has one point from the crater ring
+#The buffer distance is thus the Euclidian distance from both points
+
+ 
