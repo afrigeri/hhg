3bcba466ae41e7bba0c700848ed14c762f6ac07c
Yann Chemin <ychemin@gmail.com>
2015-09-18 22:23:45 +0200

    First version of the conversion module file, output in WKT

--- bin/raw2shp.py
+++ bin/raw2shp.py
@@ -15,23 +15,24 @@
 
 import numpy as np
 b = np.genfromtxt(r'results.txt', delimiter=',', names=True, dtype=None)
-print b[0][3].split('=')[-1], b[0][4].split('=')[-1]
-print b.shape[0] 
+print( b[0][3].split('=')[-1], b[0][4].split('=')[-1])
+print( b.shape[0] )
 
 from osgeo import ogr
 
 i = 0
-while (i < b.shape[0]):
+while (i+1 < b.shape[0]):
 	##Say we know x1, y1, x2, y2
-	x1 = b[i][3].split('=')[-1]
-	y1 = b[i][4].split('=')[-1]
-	x2 = b[i+1][3].split('=')[-1]
-	y2 = b[i+1][4].split('=')[-1]
-	wkt = "POINT (" + x1 +" " + y1 + ")"
+	x1 = float(b[i][3].split('=')[-1])
+	y1 = float(b[i][4].split('=')[-1])
+	x2 = float(b[i+1][3].split('=')[-1])
+	y2 = float(b[i+1][4].split('=')[-1])
+	wkt = "POINT (" + str(x1) +" " + str(y1) + ")"
 	pt = ogr.CreateGeometryFromWkt(wkt)
-	bufferDistance = np.sqrt(pow(x2-x1,2)+sqrt(pow(y2-y1,2))
+	bufferDistance = np.sqrt(pow(x2-x1,2)+np.sqrt(pow(y2-y1,2)))
+	print( bufferDistance)
 	poly = pt.Buffer(bufferDistance)
-	print "%s buffered by %d is %s" % (pt.ExportToWkt(), bufferDistance, poly.ExportToWkt())
+	print( "%s buffered by %d is %s" % (pt.ExportToWkt(), bufferDistance, poly.ExportToWkt()))
 	#Make sure we skip to the next tuple
 	i = i+2
 
