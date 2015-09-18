4dee68965f77845626e25be585e2eaf62ca535cb
Yann Chemin <ychemin@gmail.com>
2015-09-18 22:02:36 +0200

    Initialising the conversion module file, more

--- bin/raw2shp.py
+++ bin/raw2shp.py
@@ -8,4 +8,16 @@
 #The second line has one point from the crater ring
 #The buffer distance is thus the Euclidian distance from both points
 
+####################
+##Create a buffer from a known distance
+##Distance should be calculated by Euclidean distance between center point and crater ring point, then run the code on the whole list of points pairs
+####################
+from osgeo import ogr
+##Say we know x1, y1, x2, y2
+wkt = "POINT (" + x1 +" " + y1 + ")"
+pt = ogr.CreateGeometryFromWkt(wkt)
+bufferDistance = 500 #sqrt(pow(x2-x1,2)+sqrt(pow(y2-y1,2))
+poly = pt.Buffer(bufferDistance)
+print "%s buffered by %d is %s" % (pt.ExportToWkt(), bufferDistance, poly.ExportToWkt())
+##Now import it into a shapefile
  
